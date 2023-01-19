ea59afcf004b8ef094fbed209f7768ac4f145f09
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-01-19 18:16:47 +0100

    First vite exercise

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppTitle from './AppTitle.vue';
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    AppTitle: AppTitle
+  }
+}
+</script>
+
+<template>
+  <header>
+    <AppTitle />
+  </header>
+
+</template>
+
+
+
+<style>
+
+</style>
